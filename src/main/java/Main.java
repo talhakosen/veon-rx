cb7145a4ac1900535c5afe3d131f02af6740765d
TALHA KOSEN <talha.kosen@turkcell.com.tr>
2017-04-30 17:18:53 +0300

    create interval observable to stream sequence integer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
+import io.reactivex.*;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.observables.ConnectableObservable;
+import io.reactivex.schedulers.Schedulers;
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by tctkosen on 4/29/17.
  */
 public class Main {
-
     public static void main(String[] args) {
-        
+        Observable.interval(100, TimeUnit.MILLISECONDS).subscribeOn(Schedulers.computation());
     }
 }
